db9d876e6a5df949af26bddfd35435e8cd1af049
Aravind <aravind.sekar9797@gmail.com>
2025-01-30 12:51:18 +0530

    test

--- Test/conftest.py
+++ Test/conftest.py
@@ -9,10 +9,13 @@ from Config.config import TestData
 @pytest.fixture(params=["chrome"], scope='class')
 def init_driver(request):
     if request.param == "chrome":
+        # chrome_options = Options()
+        # chrome_options.add_argument("--headless=new")
+        # web_driver = webdriver.Chrome(options=chrome_options)
         web_driver = webdriver.Chrome()
         web_driver.maximize_window()
-        service_obj = Service(executable_path=TestData.CHROME_EXECUTABLE_PATH)
-        web_driver = webdriver.Chrome(service=service_obj)
+        # service_obj = Service(executable_path=TestData.CHROME_EXECUTABLE_PATH)
+        # web_driver = webdriver.Chrome(service=service_obj)
     if request.param == "firefox":
         service = Service(executable_path="firefox.geckodriver")
         web_driver = webdriver.Firefox(service=service)
