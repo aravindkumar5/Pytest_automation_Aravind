00bf09771dc66e04d8d61ebaa58711333be0c0d5
Aravind <aravind.sekar9797@gmail.com>
2025-01-30 14:10:52 +0530

    test98

--- Test/conftest.py
+++ Test/conftest.py
@@ -1,5 +1,6 @@
 import pytest
 from selenium import webdriver
+from selenium.webdriver.chrome.options import Options
 from selenium.webdriver.chrome.service import Service
 from selenium.webdriver.firefox import options
 
